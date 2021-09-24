b697599f0f7eb800f7d9168cc02ab964556caef2
sammyebinne <sammyebinne@gmail.com>
2021-09-24 00:35:11 -0600

    styles added 2

---
+++ yanny/src/assets/customHooks/usePost.js
@@ -0,0 +1,17 @@
+import {
+    useEffect,
+    useState
+} from 'react';
+
+const usePost = (url) => {
+
+    // fetch(url, {
+    //     method: 'POST',
+    //     headers: { "Content-Type": "application/json" },
+    //     body: JSON.stringify(blog)
+    // }).then(() => {
+    //     console.log('added')
+    // });
+}  
+
+export default usePost;
